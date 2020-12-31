366c4995e82b1268aa85a1f19874c1ef17d6bd8a
Son Dam Ngoc <Damngocson@outlook.com.vn>
2020-12-31 13:51:27 +0700

    new file:   ConsoleAppAgrs.csproj 	new file:   ConsoleAppAgrs.sln 	new file:   ParamOptions.cs 	new file:   Program.cs

---
+++ ParamOptions.cs
@@ -0,0 +1,26 @@
+﻿using MatthiWare.CommandLine.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppAgrs
+{
+    public class ParamOptions
+    {
+        [Name("u", "username"), Description("Tham số username")]
+        public string UserName { set; get; }
+
+        [Name("f", "fullname"), Description("Tham số fullname")]
+        public string FullName { set; get; }
+
+        [Name("af", "autoFillName"), Description("Khai báo thêm tham số này sẽ tự động đổ vào trường fullname")]
+        public bool AutoFillName { set; get; }
+
+
+        [Name("o", "Old"), Description("Khai báo tuổi")]
+        public int Old { set; get; }
+
+        //[Name("v", "version"), Description("Khai báo version")]
+        //public string Version { set; get; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using MatthiWare.CommandLine;
+using System;
+
+namespace ConsoleAppAgrs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var option = new CommandLineParserOptions
+            {
+                AppName = "ConsoleAppAgrs"
+            };
+
+            var parser = new CommandLineParser<ParamOptions>();
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // -v
+            parser.AddCommand<ParamOptions>().Name("-v")
+                .Required(false)
+                .Description("Show version app")
+                .OnExecuting(() =>
+                {
+                    Console.Write("Version App 7.1");
+                });
+
+            var result = parser.Parse(args);
+
+            if (result.HasErrors)
+            {
+                Console.Error.WriteLine("Parsing error ... ");
+            }
+
+            var programOption = result.Result;
+
+            if (programOption.AutoFillName == true)
+            {
+                programOption.UserName = "NguyenVanCuong";
+            }
+
+            //Console.WriteLine("Show all param : ");
+            if (!string.IsNullOrEmpty(programOption.UserName))
+                Console.WriteLine($"Show Username >> {programOption.UserName}");
+
+            if (!string.IsNullOrEmpty(programOption.FullName))
+                Console.WriteLine($"Show FullName >> {programOption.FullName}");
+
+            var old = programOption.Old;
+            if (old != 0) Console.WriteLine($"Show Old >> {programOption.Old}");
+            //if (!string.IsNullOrEmpty(programOption.Version.))
+                // Console.WriteLine($"Show Version >> {programOption.Version}");
+            if (programOption.AutoFillName == true)
+                Console.WriteLine($"Show AutoFillName is >> {programOption.AutoFillName}");
+
+
+
+        }
+
+    }
+}
